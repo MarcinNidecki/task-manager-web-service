ef26142d897f42ad6a8b3ed37bce11686b693d65
MarcinNidecki <marcin.nidecki90@gmail.com>
2019-07-12 17:56:23 +0200

    fix

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -2,8 +2,6 @@ package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
 
 import java.util.List;
 import java.util.Optional;
@@ -20,4 +18,7 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
 
     @Override
     void deleteById(Long id);
+
+    @Override
+    long count();
 }
