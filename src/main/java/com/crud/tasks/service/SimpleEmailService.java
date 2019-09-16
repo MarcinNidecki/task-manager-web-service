30807123dcaffba1e30ca5236443f0d0632d1e12
MarcinNidecki <marcin.nidecki90@gmail.com>
2019-09-16 10:36:30 +0200

    32.1

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -7,19 +7,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SimpleEmailService {
-    private static  Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
+    private static  Logger LOGGER = LoggerFactory.getLogger(MimeMessagePreparator.class);
 
     @Autowired
     private JavaMailSender javaMailSender;
+    @Autowired
+    private MailCreatorService mailCreatorService;
 
     public void send(Mail mail) {
         LOGGER.info("Starting emial preparation...");
         try {
-            javaMailSender.send(createMailMessage(mail));
+            javaMailSender.send(createMimeMessage(mail));
             LOGGER.info("Email has been sent.");
         }catch (MailException e) {
             LOGGER.error("Failed to process email sending: ", e.getMessage(),e);
@@ -35,6 +39,15 @@ public class SimpleEmailService {
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
         return mailMessage;
+    }
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
     }
 
 
--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -3,12 +3,15 @@ package com.crud.tasks.service;
 import com.crud.tasks.client.TrelloClient;
 import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.dto.CreatedTrelloCardDto;
+import com.crud.tasks.domain.Mail;
 import com.crud.tasks.dto.TrelloBoardDto;
 import com.crud.tasks.dto.TrelloCardDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static java.util.Optional.ofNullable;
 
 @Service
 public class TrelloService {
@@ -25,15 +28,12 @@ public class TrelloService {
     private static final String SUBJECT = "Tasks: New Trello card";
 
     public CreatedTrelloCardDto createTrelloCard(final TrelloCardDto trelloCardDto) {
-      /*   CreatedTrelloCardDto newCard = trelloClient.createNewCard(trelloCardDto);
-        dfdasd
+        CreatedTrelloCardDto newCard = trelloClient.createNewCard(trelloCardDto);
         ofNullable(newCard).ifPresent(card -> emailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT, "New card: " + card.getName() + " has been created on your Trello account")));
 
-        return newCard;  */
-      return null;
+        return newCard;
     }
 
     public List<TrelloBoardDto> fetchTrelloBoards() {
-        return trelloClient.getTrelloBoards();
     }
 }
